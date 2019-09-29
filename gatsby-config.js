a9cc6376748e0df6e637f6fd9a82129dd9bda4dd
George Wilde <george@wildecreations.co.uk>
2019-09-29 11:33:04 +0100

    Add blog template and markdown support to Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 }
